b02ca3e238da04924ce118816d5400be9e06d036
OmarH435 <72316100+OmarH435@users.noreply.github.com>
2024-08-31 15:41:52 -0600

    Add files via upload
    Ejercicio que al ingresar una cadena de texto, la imprime en reversa

---
+++ pritn.reverse.py
@@ -0,0 +1,14 @@
+cadena = input("Introduzca una cadena de texto: ")
+
+reverso = ""
+
+for letra in cadena:
+
+    reverso = letra + reverso
+
+# Mostrar la cadena en reverso
+
+print("El reverso de la cadena es:", reverso)
+
+
+# usar el ciclo for al reverso, y el contador, debe de ir restando
